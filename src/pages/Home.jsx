8ac469fde5378982200ef09d4c8f724c7977cafb
Kushchina Alena <alenakushchina@gmail.com>
2024-12-29 22:10:49 +0100

    the project structure has been created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import '../styles/app.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h1>Chez vous, partout et ailleurs</h1>
+    </div>
+  );
+};
+
+export default Home;
